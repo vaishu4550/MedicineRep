8a6ff72b1633391ee02e98c1a3be5a6a7f5845a3
vaishu4550 <2106762@cognizant.com>
2022-06-23 14:40:16 +0530

    first commit

---
+++ Controllers/RepScheduleController.cs
@@ -0,0 +1,62 @@
+﻿using log4net.Config;
+using MedicalRepresentativeSchedule.models;
+using MedicalRepresentativeSchedule.Providers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Castle.Core.Internal;
+
+namespace MedicalRepresentativeSchedule.Controllers
+{
+   
+    [Route("[controller]")]
+    [ApiController]
+    public class RepScheduleController : ControllerBase
+    {
+        private readonly IRepScheduleProvider _repScheduleProvider;
+       
+
+        //static readonly log4net.ILog _log4net = log4net.LogManager.GetLogger(typeof(RepScheduleController));
+        public RepScheduleController(IRepScheduleProvider repSchedule)
+        {
+            this._repScheduleProvider = repSchedule;
+        }
+        [HttpGet("detail")]
+        public string[] detail()
+        {
+            var u = new string[] { "a", "b", "c" };
+            return u;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get(DateTime startDate)
+        {
+            try
+            {
+                var res = await _repScheduleProvider.GetRepScheduleAsync(startDate);
+                if (!res.IsNullOrEmpty())
+                {
+                  
+                    return new OkObjectResult(res);
+                }
+                else
+                {
+                   
+                    return NotFound("schedule not received");
+                }
+            }
+            catch (Exception e)
+            {
+               
+                return StatusCode(500);
+            }
+        }
+    }
+}
---
+++ repositories/RepScheduleRepository.cs
@@ -0,0 +1,56 @@
+﻿using MedicalRepresentativeSchedule.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MedicalRepresentativeSchedule.repositories 
+{
+
+    public class RepScheduleRepository : IRepScheduleRepository
+    {
+        private static List<Doctor> doctors;
+        private static List<RepresentativeDetails> representatives;
+        List<DoctorDTO> docDetails = new List<DoctorDTO>();
+        List<RepresentativeDetailsDTO> repDetails = new List<RepresentativeDetailsDTO>();
+        public RepScheduleRepository()
+        {
+            doctors = new List<Doctor>()
+             {
+               new Doctor { Name = "Doctor1",ContactNumber=987654321 , TreatingAilment="Orthopaedics"},
+               new Doctor { Name = "Doctor2",ContactNumber=987654321 , TreatingAilment="General"},
+               new Doctor { Name = "Doctor3",ContactNumber=987654321 , TreatingAilment="Gynecology"},
+               new Doctor { Name = "Doctor4",ContactNumber=987654321 , TreatingAilment="General"},
+               new Doctor { Name = "Doctor5",ContactNumber=987654321 , TreatingAilment="Gynecology"},
+             };
+
+            representatives = new List<RepresentativeDetails>()
+            {
+                new RepresentativeDetails{RepresentativeName= "Ramesh" },
+                new RepresentativeDetails{RepresentativeName= "Suresh" },
+                new RepresentativeDetails{RepresentativeName= "Girish" }
+            };
+
+
+        }
+        public List<DoctorDTO> GetDoctorDetails()
+        {
+            foreach (var i in doctors)
+            {
+                docDetails.Add(new DoctorDTO { Name = i.Name, ContactNumber = i.ContactNumber, TreatingAilment = i.TreatingAilment });
+            }
+            return docDetails;
+        }
+        public List<RepresentativeDetailsDTO> GetRepresentativesDetails()
+        {
+            foreach (var i in representatives)
+            {
+                repDetails.Add(new RepresentativeDetailsDTO { RepresentativeName = i.RepresentativeName });
+            }
+            return repDetails;
+        }
+
+    }
+        
+    
+}
